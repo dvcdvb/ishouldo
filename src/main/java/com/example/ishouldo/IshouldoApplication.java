12ad1340c8f273ad2fe86b7be53fbd78bc17d5b6
fab <fabiopalma2504@gmail.com>
2025-09-24 22:01:03 +0200

    initial commit

---
+++ src/main/java/com/example/ishouldo/IshouldoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ishouldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IshouldoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IshouldoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ishouldo/IshouldoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ishouldo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IshouldoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
